6982c06436bbb9c8e318d8a5566739170ec07500
DianaGaleano245 <emmaymaggi@gmail.com>
2022-03-09 15:40:12 -0300

    Creacion RepasoPersonal

---
+++ Persona.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RepasoPersona
+{
+    public class Persona
+    {
+        public string Nombre {get; set;}
+        public String Apellido {get; set;}
+        public double Efectivo {get; private set;}
+    }
+    public Persona () => efectivo = 0;
+    
+    public void Debitar (double monto)
+    {
+        if (monto <=0)
+        throw new Exception ("El monto debe ser > a 0");
+    
+    
+        If (efectivo < monto);
+        throw new Exception ("saldo insuficiente");
+    
+    Efectivo =monto ;
+    }
+}
